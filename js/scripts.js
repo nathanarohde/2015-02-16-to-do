d364067e3e3bbf3e3028311b89412e773ef73f88
Monty Rohde & Dex Fitch <student@epicodus.com>
2015-02-16 12:07:57 -0800

    Experiment with completed tasks function

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,31 @@
-$(document).ready(function () {
+$(document).ready(function() {
   $('form#new-list').submit(function(event) {
     event.preventDefault();
 
     var inputTask = $('input#new-task').val();
     var newList = { task: inputTask };
 
-    $('ul#tasks').append('<li><span class="task">' + newList.task + '</span></li>');
+
+    $('ul#tasks').append('<li><input type="checkbox" class="completed-task">' + ' ' + '<span class="task">' + newList.task + '</span></li>');
 
     $('input#new-task').val('');
 
-    $('.task').last().click(function() {
-      $('#show-list').show();
-      $('.task').text(newList.task);
-    });
+
+
   });
+
+// $('.task').last().click(function() {
+//   $('#show-list').show();
+//   $('.task').text(newList.task);
+// });
+
+
+  // $('.completed-task').change(function() {
+  //
+  //   if ($(this).is(':checked')) {
+  //     $('#show-completed').show();
+  //   };
+  //
+  // });
+
 });
