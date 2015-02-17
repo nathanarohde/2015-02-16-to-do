3ac1697d89c380eb00a6669794439a64d0ffa35c
Monty Rohde & Dex Fitch <student@epicodus.com>
2015-02-16 16:02:25 -0800

    Adds selected tasks to completed tasks.

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,31 @@ $(document).ready(function() {
     var inputTask = $('input#new-task').val();
     var newList = { task: inputTask };
 
-
-    $('ul#tasks').append('<li><input type="checkbox" class="completed-task">' + ' ' + '<span class="task">' + newList.task + '</span></li>');
+    $('ul#tasks').append('<li><input type="checkbox" class="completed-task" value= "'+newList.task+'" >' + ' ' + '<span class="task">' + newList.task + '</span></li>');
 
     $('input#new-task').val('');
 
-
-
   });
+
+  $('#save_value').click(function() {
+    var newCompletedTaskList = []
+    $(':checkbox:checked').each(function(i) {
+        newCompletedTaskList[i] = $(this).val();
+    });
+
+    newCompletedTaskList.forEach(function(task) {
+    $('ul#completed-tasks').append('<li>'+ task +'</li>');
+    });
+  });
+
+  //
+
+  // });
+
+  // $('form#completed-list').submit(function(event) {
+  //   event.preventDefault();
+  // });
+});
 
 // $('.task').last().click(function() {
 //   $('#show-list').show();
@@ -27,5 +44,3 @@ $(document).ready(function() {
   //   };
   //
   // });
-
-});
